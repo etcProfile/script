484030f5fc9f8a47a963894a75ac8253369c6b75
luosf <44489087+etcProfile@users.noreply.github.com>
2023-03-18 22:17:11 +0800

    Update RNA_seq_script.sh

--- RNA_seq_script.sh
+++ RNA_seq_script.sh
@@ -59,10 +59,6 @@ for(i in names(res)){
 }
 rld <- rlogTransformation(dds)
 
-library(RColorBrewer)
-(mycols <- brewer.pal(8, "Dark2")[1:length(unique(condition))])
-sampleDists <- as.matrix(dist(t(assay(rld))))
-
 #valcano_plot
 library(ggplot2)
 resdata$change <- as.factor(
@@ -85,42 +81,6 @@ valcano <- ggplot(data=resdata, aes(x=log2FoldChange, y=-log10(padj), color=chan
 	geom_hline(yintercept=-log10(0.01), lty=2, col="gray", lwd=0.5)
 
 topptx(valcano,filenames="valcano.pptx",width=6,hight=6)
-
-########sample_pca_plot#######
-#https://github.com/sachitsaksena/nfkb-tag-seq/blob/master/rld_pca.R
-library(gplots)
-rld_pca <- function (rld, intgroup = "condition", ntop = 500, colors=NULL, legendpos="bottomleft", main="PCA Biplot", textcx=1, ...) {
-  require(genefilter)
-  require(calibrate)
-  require(RColorBrewer)
-  rv = rowVars(assay(rld))
-  select = order(rv, decreasing = TRUE)[seq_len(min(ntop, length(rv)))]
-  pca = prcomp(t(assay(rld)[select, ]))
-  fac = factor(apply(as.data.frame(colData(rld)[, intgroup, drop = FALSE]), 1, paste, collapse = " : "))
-  print(length(fac))
-  if (is.null(colors)) {
-    if (nlevels(fac) >= 3) {
-      colors = brewer.pal(nlevels(fac), "Paired")
-    }   else {
-      colors = c("black", "red")
-    }
-  }
-  pc1var <- round(summary(pca)$importance[2,1]*100, digits=1)
-  pc2var <- round(summary(pca)$importance[2,2]*100, digits=1)
-  pc1lab <- paste0("PC1 (",as.character(pc1var),"%)")
-  pc2lab <- paste0("PC1 (",as.character(pc2var),"%)")
-  plot(PC2~PC1, data=as.data.frame(pca$x), bg=colors[fac], pch=21, xlab=pc1lab, ylab=pc2lab, main=main, ...)
-  # with(as.data.frame(pca$x), textxy(PC1, PC2, labs=rownames(as.data.frame(pca$x)), cex=textcx))
-  # legend(legendpos, legend=levels(fac), col=colors, pch=20, ncol = 4)
-  # rldyplot(PC2 ~ PC1, groups = fac, data = as.data.frame(pca$rld),
-  #       pch = 16, cerld = 2, aspect = "iso", col = colours, main = draw.key(key = list(rect = list(col = colours),
-  #                                                                                   terldt = list(levels(fac)), rep = FALSE)))
-  return(pca)
-}
-pdf(file="group_pca.pdf")
-rld_pca(rld, colors=mycols, intgroup="condition", xlim=c(-300, 300),ylim=c(-300,300),textcx=0.5)
-dev.off()
-
 ############################################################################################################
 #Convert featureCounts result "Count.txt" to "Gene_expression_FPKM_or_TPM".
 ## First you could use wheel package to convert counts result to FPKM, then Manual convert to the TPM format.
@@ -157,38 +117,3 @@ tpm <- t(t(rpk)/colSums(rpk) * 1000000)
 write.csv(avg_tpm,paste0(prefix,"_avg_tpm.csv"))
 write.csv(tpm,paste0(prefix,"_tpm.csv"))
 ###################################################################################################
-## functions for rpkm and tpm
-## from https://gist.github.com/slowkow/c6ab0348747f86e2748b#file-counts_to_tpm-r-L44
-## from https://www.biostars.org/p/171766/
-rpkm <- function(counts, lengths) {
-  rate <- counts / lengths
-  rate / sum(counts) * 1e9
-}
-
-tpm <- function(counts, lengths) {
-  rate <- counts / lengths
-  rate / sum(rate) * 1e6
-}
-
-## read table from featureCounts output
-args <- commandArgs(T)
-tag <- tools::file_path_sans_ext(args[1])
-ftr.cnt <- read.table(args[1], sep="\t", stringsAsFactors=FALSE,header=TRUE)
-library(dplyr)
-library(tidyr)
-
-ftr.rpkm <- ftr.cnt %>%
-  gather(sample, cnt, 7:ncol(ftr.cnt)) %>%
-  group_by(sample) %>%
-  mutate(rpkm=rpkm(cnt, Length)) %>%
-  select(-cnt) %>%
-  spread(sample, rpkm)
-write.table(ftr.rpkm, file=paste0(tag, "_rpkm.txt"), sep="\t", row.names=FALSE, quote=FALSE)
-
-ftr.tpm <- ftr.cnt %>%
-  gather(sample, cnt, 7:ncol(ftr.cnt)) %>%
-  group_by(sample) %>%
-  mutate(tpm=tpm(cnt, Length)) %>%
-  select(-cnt) %>%
-  spread(sample, tpm)
-write.table(ftr.tpm, file=paste0(tag, "_tpm.txt"), sep="\t", row.names=FALSE, quote=FALSE)
