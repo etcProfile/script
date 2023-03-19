600d60775fc3f7966e287f24d3cc3bdd250c3872
luosf <44489087+etcProfile@users.noreply.github.com>
2023-03-19 14:23:56 +0800

    Add files via upload

---
+++ paml/paml_test.sh
@@ -0,0 +1,37 @@
+```
+#!/bin/bash
+# This script was written by Luo Shaofan on May 19, 2022
+# Define variables
+seagrass_path="/home/lsf/seagrass/chloroplast/New_cp/kaks"
+paml_path="/home/lsf/seagrass/chloroplast/New_cp/kaks/branch_model"
+
+# List all directories
+ls -l | grep ^d | awk '{print$9}' | while read id
+do
+# Merge fasta files
+concatenate_fasta.py *no_stop_codon.fasta --separate -o all_${id}_cds.fasta 
+concatenate_fasta.py *protein.fasta --separate -o all_${id}_pep.fasta 
+# Perform multiple sequence alignment
+mafft --retree 2 --maxiterate 1000 --thread 40 all_${id}_pep.fasta > all_${id}_pep.aln.fasta 
+# Convert protein sequence alignment results to nucleic acid sequence alignment results
+pal2nal.pl all_${id}_pep.aln.fasta all_${id}_cds.fasta -nogap -codontable 11 -output paml > cds.pal2nal
+# Create folders
+mkdir -p ${paml_path}/alrt_model
+mkdir -p ${paml_path}/null_model
+# Copy files
+cp ./*.pal2nal ${paml_path}/alrt_model
+cp ./*.pal2nal ${paml_path}/null_model
+cp ${seagrass_path}/all_cds.aln.fasta.treefile ${paml_path}/null_model
+cp ${seagrass_path}/all_cds.aln.fasta.treefile ${paml_path}/alrt_model
+cp ${seagrass_path}/codeml.ctl ${paml_path}/null_model
+cp ${seagrass_path}/codeml_alrt.ctl ${paml_path}/alrt_model
+# Enter folders
+cd ${paml_path}/${id}/paml/null_model
+# Run codeml
+codeml codeml.ctl
+# Enter folders
+cd ${paml_path}/${id}/paml/alrt_model
+# Run codeml
+codeml codeml_alrt.ctl
+done
+```
