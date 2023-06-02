5dd2c03937462a44326c72962152c57dbe5a2ef9
luosf <44489087+etcProfile@users.noreply.github.com>
2023-06-02 16:08:13 +0800

    Update paml_test.sh

--- paml/paml_test.sh
+++ paml/paml_test.sh
@@ -9,12 +9,16 @@ paml_path="/home/lsf/seagrass/chloroplast/New_cp/kaks/branch_model"
 ls -l | grep ^d | awk '{print$9}' | while read id
 do
 # Merge fasta files
+#perl -pe '$. > 1 and /^>/ ? print "\n" : chomp' ${gene}.fasta > ${gene}.tmp.fasta
+#sed -e "s/TGA$//" -e "s/TAA$//" -e "s/TAG$//" ${gene}.tmp.fasta > ${id}_${gene}_no_stop_codon.fasta
+#transeq -sequence ${id}_${gene}_no_stop_codon.fasta -outseq ${id}_${gene}_protein.fasta
+#sed -i "s/\_1//g" ${id}_${gene}_protein.fasta
+
 concatenate_fasta.py *no_stop_codon.fasta --separate -o all_${id}_cds.fasta 
 concatenate_fasta.py *protein.fasta --separate -o all_${id}_pep.fasta 
 # Perform multiple sequence alignment
 mafft --retree 2 --maxiterate 1000 --thread 40 all_${id}_pep.fasta > all_${id}_pep.aln.fasta 
-# Convert protein sequence alignment results to nucleic acid sequence alignment results
-pal2nal.pl all_${id}_pep.aln.fasta all_${id}_cds.fasta -nogap -codontable 11 -output paml > cds.pal2nal
+#pal2nal.pl all_${id}_pep.aln.fasta all_${id}_cds.fasta -nogap -codontable 11 -output paml > cds.pal2nal
 # Create folders
 mkdir -p ${paml_path}/alrt_model
 mkdir -p ${paml_path}/null_model
